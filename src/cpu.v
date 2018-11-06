ab3a5d81b498f8f7cc3cb31c6390c38ccbe6c375
Piyush Itankar <pitankar@gmail.com>
2018-11-06 06:47:22 +0000

    Adds Memory module implementation for Data, code and register memory
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- src/cpu.v
+++ src/cpu.v
@@ -1,16 +1,10 @@
 /*
  * Implementation of a 8 bit CPU with 4 basic instructions
  */
-
 module cpu #(parameter WORD_SIZE=8, ADDR_WIDTH=8)
-            (clk, wr_en, addr_data, addr_data_in, addr_display, data_display, data_in_display);
-    input clk;
-    input wr_en;
-    input addr_data;
-    input [WORD_SIZE-1:0] addr_data_in;
-    output [13:0] addr_display;
-    output [13:0] data_display;
-    output [13:0] data_in_display;
+            (input clk, wr_en, addr_data,
+             input [WORD_SIZE-1:0] addr_data_in,
+             output [13:0] addr_display, data_display, data_in_display);
 
     wire [WORD_SIZE-1:0] data_out;
     reg [ADDR_WIDTH-1:0] addr;
@@ -30,5 +24,6 @@ module cpu #(parameter WORD_SIZE=8, ADDR_WIDTH=8)
     seven_seg_display data_in_display_high(clk, addr_data_in[7:4], data_in_display[13:7]);
     seven_seg_display data_in_display_low (clk, addr_data_in[3:0], data_in_display[6:0]);
 
-    memory ram(clk, wr_en, addr, addr_data_in, data_out);
+    // Create the RAM for Testing
+    module_data_memory ram(clk, wr_en, addr, addr_data_in, data_out);
 endmodule
--- src/memory.v
+++ src/memory.v
@@ -1,21 +1,64 @@
 /*
- * Implements a simple memory block
+ * Implementation of Data memory module
  */
+module module_data_memory #(parameter WORD_SIZE=32, ADDRESS_BITS=32, MEMORY=1024)
+                           (input clk, wr_en,
+                            input [ADDRESS_BITS-1:0] addr,
+                            input [WORD_SIZE-1:0] data_in,
+                            output reg [WORD_SIZE-1:0] data_out);
 
-module memory #(parameter WORD_SIZE=8, ADDR_WIDTH=8) (clk, wr_en, addr, data_in, data_out);
-	input clk;
-	input wr_en;
-	input [WORD_SIZE-1:0]  data_in;
-	input [ADDR_WIDTH-1:0] addr;
-	output reg [WORD_SIZE-1:0]  data_out;
+    // Actual Memory declaration
+    reg [WORD_SIZE-1:0] mem [MEMORY - 1 : 0];
 
-	// Actual Memory declaration
-	reg [WORD_SIZE-1:0] mem [2**ADDR_WIDTH - 1 : 0];
+    // Write to memory if WR_EN is TRUE
+    always @(posedge clk) begin
+      if (wr_en) data_out <= mem[addr];
+      else mem[addr] <= data_in;
+    end
+endmodule
 
-	// Write to memory if WR_EN is TRUE
-	always @(posedge clk) begin
-	  if (wr_en) data_out <= mem[addr];
-	  else mem[addr] <= data_in;
-	end
+/*
+ * Implementation of Instruction memory module
+ */
+module module_instruction_memory #(parameter ADDRESS_BITS=32, MEMORY=1024, WORD_SIZE=32)
+                                  (input clk,
+                                   input [ADDRESS_BITS-1:0] addr,
+                                   input wr_en,
+                                   input [WORD_SIZE-1:0] code,
+                                   output reg [WORD_SIZE-1:0] instruction);
 
+    // Create the memory
+    reg [WORD_SIZE-1:0] instruction_memory [0:MEMORY-1];
+
+    // Write the instruction to output or save an incoming port.
+    always @(posedge clk) begin
+        if (wr_en) instruction_memory[addr] <= code;
+        else instruction <= instruction_memory[addr];
+    end
+endmodule
+
+/*
+ * Implementation of Register bank
+ */
+module module_register_bank #(parameter REGISTER_COUNT=32, REGISTER_WIDTH=32, ADDRESS_BITS=5)
+                             (input clk,
+                              input [ADDRESS_BITS-1:0] rd_addr_1, rd_addr_2, wr_addr,
+                              input wr_en,
+                              input [REGISTER_WIDTH-1:0] data_in,
+                              output reg [REGISTER_WIDTH-1:0] d_out_1, d_out_2);
+
+    // Create a Register Bank
+    reg [REGISTER_WIDTH-1:0] reg_file [0:REGISTER_COUNT-1];
+
+    always @(posedge clk) begin
+        // Output the content of the selected registers
+        d_out_1 <= reg_file[rd_addr_1];
+        d_out_2 <= reg_file[rd_addr_2];
+
+        // Last register is always Zero!
+        reg_file[REGISTER_COUNT-1] <= 0;
+
+        // Load in the appropriate register when wr_en is true
+        if (wr_en) reg_file[wr_addr] <= data_in;
+    end
 endmodule
