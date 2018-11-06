73a1b6181003940813c1ba0797fe6533478cd26a
Piyush Itankar <pitankar@gmail.com>
2018-11-06 09:04:51 +0000

    Adds module for status register
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- src/special_registers.v
+++ src/special_registers.v
@@ -1,4 +1,24 @@
 /*
+ * Implements Status Register
+ * bit 0: Zero Flag
+ */
+module module_sr #(parameter WORD_SIZE=32)
+                  (input clk, reset, zero_fg,
+                   output reg [WORD_SIZE-1:0] sr_out);
+
+    // Declare the Program counter
+    reg [WORD_SIZE-1:0] sr;
+
+    // On every clock cycle increment the PC
+    // if an address is provided then load that,
+    // On reset make PC point to the 0th location.
+    always @(posedge clk) begin
+        if (zero_fg) sr[0] <= 1'b1;
+        else sr <= 0;
+    end
+endmodule
+
+/*
  * Implements Program Counter
  */
 module module_pc #(parameter WORD_SIZE=32)
