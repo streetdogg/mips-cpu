b91489f67d1e8a54110e1ed0a87d75e757de3db6
Piyush Itankar <pitankar@gmail.com>
2018-11-07 21:04:21 +0530

    Connects all blocks together TODO: write testbench for each module and erify the implementation
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- src/cpu.v
+++ src/cpu.v
@@ -2,33 +2,31 @@
  * Implements PC testing module
  */
 module cpu #(parameter WORD_SIZE=8, ADDR_WIDTH=8)
-            (input clk, reset, load_addr,
+            (input sys_clk, reset, load_addr, hlt,
 				 input [31:0] addr,
              output [13:0] data_display);
 
-    wire [WORD_SIZE-1:0] data_out, instruction, data, alu_data, imm_data, ro1, ro2, status;
-    wire rst, load, ra, rb, rc, alu_op, wr_en, sub_en, zero;
+    wire [WORD_SIZE-1:0] inst_addr, instruction, data, alu_data, imm_data, ro1, ro2, status;
+    wire clk, rst, load, ra, rb, rc, alu_op, wr_en, sub_en, zero;
 
-    reg [31:0] ad = 4;
+    reg [31:0] ad = 0;
     assign rst = ~reset;
     assign load = ~load_addr;
 
-    seven_seg_display data_display_high(clk, data_out[7:4], data_display[13:7]);
-    seven_seg_display data_display_low (clk, data_out[3:0], data_display[6:0]);
-
     // choose between ALU operation and immediate value
     assign data = (alu_op) ? alu_data : imm_data;
+    assign clk = (hlt) ? 1'b0 : ~sys_clk;
 
     // Program counter
     module_pc pc(.clk(clk),
                  .reset(rst),
                  .wr_en(load),
                  .addr(ad),
-                 .pc_out(data_out));
+                 .pc_out(inst_addr));
 
     // IRAM: stores all instructions
     module_instruction_memory iram(.clk(clk),
-                                   .addr(data_out),
+                                   .addr(inst_addr),
                                    .instruction(instruction));
 
     // Program counter
@@ -66,5 +64,9 @@ module cpu #(parameter WORD_SIZE=8, ADDR_WIDTH=8)
                            .wr_en(wr_en),
                            .sub(sub_en),
                            .alu_op(alu_op));
+
+
+    seven_seg_display data_display_high(clk, ro1[7:4], data_display[13:7]);
+    seven_seg_display data_display_low (clk, ro1[3:0], data_display[6:0]);
 
 endmodule
